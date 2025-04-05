b8ca2db4f554453eebc8f043d68a16b170021e59
JiyeSun <jiyesun217@gmail.com>
2025-04-05 06:57:15 -0400

    Update rag_helper.py

--- rag_helper.py
+++ rag_helper.py
@@ -6,7 +6,8 @@ from openai import OpenAI
 import streamlit as st  
 import openai 
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+#openai.api_key = os.getenv("OPENAI_API_KEY")
 #client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 DOC_CHUNKS = []
@@ -27,13 +28,20 @@ def extract_text_chunks(folder_path="data", chunk_size=300) -> List[str]:
                             chunks.append(chunk)
     return chunks
 
-
+def get_embeddings(texts: List[str]) -> np.ndarray:
+    response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=texts
+    )
+    return np.array([r.embedding for r in response.data])
+'''
 def get_embeddings(texts: List[str]) -> np.ndarray:
     response = openai.Embedding.create(
         model="text-embedding-3-small",
         input=texts
     )
     return np.array([r["embedding"] for r in response["data"]])
+'''
 
 def init_knowledge_base(folder_path="data"):
     global DOC_CHUNKS, CHUNK_EMBEDDINGS
