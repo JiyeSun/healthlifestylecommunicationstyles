32e689fe63545622eab8c550fa5b1235bcf22350
JiyeSun <jiyesun217@gmail.com>
2025-04-09 03:15:59 -0400

    Update healthmate_app.py

--- healthmate_app.py
+++ healthmate_app.py
@@ -298,7 +298,7 @@ debug = True
 if debug:
     #st.markdown("### User Info")
     st.markdown(f"- **Participant ID**: `{pid}`")
-    st.markdown("**Tips:**")
+    #st.markdown("**Tips:**")
     st.markdown("- **To begin, please type ‘Hi, healthmate.’ in the input box and click the Send button.**")
     #st.markdown("- **You will be asked about your current diet and exercise habits, followed by a question about your recent health goals, if any. After you provide this information, the chatbot will generate a weekly plan for you.**")
     #st.markdown("- **Please be patient—when the upper-right corner of the page shows ‘Running,’ it means the chatbot is processing your input.**")
