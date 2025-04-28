5372c26534879cda6ba85dcbc930432bb5d6b91e
JiyeSun <jiyesun217@gmail.com>
2025-04-27 22:54:04 -0400

    Update healthmate_app.py

--- healthmate_app.py
+++ healthmate_app.py
@@ -92,13 +92,13 @@ style_prompt = PROMPT_DICT.get(cond, PROMPT_DICT["1"])
 debug = True
 if debug:
     #st.markdown("### User Info")
-    st.markdown(f"- **Participant ID**: `{pid}`")
+    #st.markdown(f"- **Participant ID**: `{pid}`")
     #st.markdown("**Tips:**")
     st.markdown("- **To begin, please type ‘Hi, healthmate.’ in the input box and click the Send button.**")
     #st.markdown("- **You will be asked about your current diet and exercise habits, followed by a question about your recent health goals, if any. After you provide this information, the chatbot will generate a weekly plan for you.**")
     #st.markdown("- **Please be patient—when the upper-right corner of the page shows ‘Running,’ it means the chatbot is processing your input.**")
     #st.markdown(f"- **Condition**: `{cond}`")
-    st.markdown("---")
+    #st.markdown("---")
 
 # ====== Main Interaction (with Enter to Send + Input Clear) ======
 with st.form("chat_form", clear_on_submit=True):
